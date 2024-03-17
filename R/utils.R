9ace2c19bce76ce2687606ac1b76202dea125d62
fursham_h <fursham.h@gmail.com>
2024-03-17 11:08:41 +0000

    Added check for GTF structure

--- R/utils.R
+++ R/utils.R
@@ -18,3 +18,18 @@
          sep, x$gene_id,
          sep, GenomicRanges::strand(x))
 }
+
+is_gtf <- function(x) {
+  if (is(x, "GRanges")) {
+    x <- as.data.frame(x)
+    if (all(c("type", "gene_id", "transcript_id") %in% names(x))) {
+      x <- x %>%
+        dplyr::select(type, gene_id, transcript_id) %>%
+        dplyr::distinct()
+      if (nrow(x) > 1) {
+        return(TRUE)
+      }
+    }
+  }
+  return(FALSE)
+}
